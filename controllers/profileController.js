919b3700b31ba8e308d0b7c92d6298ffc74b90e8
Hizkia Jonathan Budiana <hizkia.jonathanb@gmail.com>
2025-06-04 15:30:48 +0700

    ..

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -4,6 +4,8 @@ class ProfileController {
   static async checkProfile(req, res) {
     try {
       const userId = req.session.usedId;
+      const { notification } = req.query;
+
       const checkProfile = await UserProfile.findOne({
         where: { UserId: userId },
       });
@@ -11,7 +13,22 @@ class ProfileController {
       if (!checkProfile) {
         res.redirect(`/profile/set/${userId}`);
       }
-      res.render("profile");
+
+      res.render("profile", { notification });
+    } catch (error) {
+      console.log(error);
+      res.send(error);
+    }
+  }
+
+  static async backButton(req, res) {
+    try {
+      if (req.session.role === "roleA") {
+        res.redirect("/roleA");
+      }
+      if (req.session.role === "roleB") {
+        res.redirect("/roleB");
+      }
     } catch (error) {
       console.log(error);
       res.send(error);
@@ -20,10 +37,10 @@ class ProfileController {
 
   static async setProfileForm(req, res) {
     try {
-      // const {error} = req.query
       const { userId } = req.params;
+      const { error } = req.query;
 
-      res.render(`setProfileForm`);
+      res.render(`setProfileForm`, { error, userId });
     } catch (error) {
       console.log(error);
       res.send(error);
@@ -42,24 +59,25 @@ class ProfileController {
         photoUrl,
         UserId: userId,
       });
-
-      if (req.session.role === "roleA") {
-        res.redirect("/roleA");
-      }
-      if (req.session.role === "roleB") {
-        res.redirect("/roleB");
-      }
+      const msg = "You succesfully created profile";
+      res.redirect(`/profile?notification=${msg}`);
     } catch (error) {
-      console.log(error);
-      res.send(error);
+      if (error.name === "SequelizeValidationError") {
+        const err = error.errors.map((el) => el.message);
+        res.redirect(`/profile/set?error=${err}`);
+      } else {
+        console.log(error);
+        res.send(error);
+      }
     }
   }
 
   static async editProfileForm(req, res) {
     try {
       const { userId } = req.params;
+      const { error } = req.query;
 
-      res.render(`editProfileForm`);
+      res.render(`editProfileForm`, { error, userId });
     } catch (error) {
       console.log(error);
       res.send(error);
@@ -76,17 +94,17 @@ class ProfileController {
         { where: { UserId: userId } }
       );
 
-      if (req.session.role === "roleA") {
-        res.redirect("/roleA");
-      }
-      if (req.session.role === "roleB") {
-        res.redirect("/roleB");
-      }
+      const msg = "You succesfully created profile";
+      res.redirect(`/profile?notification=${msg}`);
     } catch (error) {
-      console.log(error);
-      res.send(error);
+      if (error.name === "SequelizeValidationError") {
+        const err = error.errors.map((el) => el.message);
+        res.redirect(`/profile/edit?error=${err}`);
+      } else {
+        console.log(error);
+        res.send(error);
+      }
     }
   }
-  
 }
 module.exports = ProfileController;
