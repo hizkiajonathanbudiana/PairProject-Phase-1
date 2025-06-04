a8c7c20306d6b892c3028349d76c44ada580b125
Hizkia Jonathan Budiana <hizkia.jonathanb@gmail.com>
2025-06-05 04:51:22 +0700

    DONE SIR

--- seeders/20250604175652-seed-user.js
+++ seeders/20250604175652-seed-user.js
@@ -1,29 +1,27 @@
-'use strict';
-const fs = require('fs').promises;
-const bcrypt = require('bcryptjs');
-
+"use strict";
+const fs = require("fs").promises;
+const bcrypt = require("bcryptjs");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     let dbUser = JSON.parse(await fs.readFile("./data/user.json", "utf-8"));
 
-    const salt = 10
-    
-    for (let el of dbUser) {
-      el.password = await bcrypt.hash(el.password, salt); 
-      el.createdAt = el.updatedAt = new Date();
-    }
+    const salt = 10;
 
-    await queryInterface.bulkInsert('Users', dbUser)
+    dbUser.forEach(el => {
+      el.password = bcrypt.hashSync(el.password, salt);
+      el.createdAt = el.updatedAt = new Date();
+    });
+
+    await queryInterface.bulkInsert("Users", dbUser);
   },
 
   async down(queryInterface, Sequelize) {
-
-    await queryInterface.bulkDelete('Users', null, {
+    await queryInterface.bulkDelete("Users", null, {
       restartIdentity: true,
       truncate: true,
-      cascade: true
-    })
-  }
+      cascade: true,
+    });
+  },
 };
